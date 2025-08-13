8f54484ec8ec496f914411f8894a7e80f5899abf
Cid Franco Ordoñez <cid5ordonez@gmail.com>
2025-08-13 17:23:45 +0800

    Update streamlit_app.py
    Put the JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
     
     #st.write(ingredients_string)
-
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """' , '"""+ name_on_order+ """')"""
@@ -49,3 +48,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response).json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
